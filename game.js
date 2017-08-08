5fd9d646b40a173c396def4c07554e9cd3c7a50f
Chris Thamo <chrisdev2016@users.noreply.github.com>
2017-08-08 13:56:31 -0400

    blah

--- game.js
+++ game.js
@@ -2,27 +2,30 @@ $(document).ready(function () {
 
     var mC = 0;
     var cC = 0;
+
     document.getElementById("myCounter").innerHTML = mC;
     document.getElementById("computerCounter").innerHTML = cC;
 
-    $("#start").click(function () {
-
-        var computerSpeed = Math.random() * 1000;
+      var computerSpeed = Math.random() * 1000;
         var mySpeed = Math.random() * 1000;
 
-        if (mC != 0  || cC != 0)
-        {
+    $("#start").click(function () {
+
+        // var computerSpeed = Math.random() * 1000;
+        // var mySpeed = Math.random() * 1000;
+
+        if (mC != 0 || cC != 0) {
             // $("#box1").animate({ "bottom": "-=420px" }, 0)
             $("#box2").animate({ "bottom": "-=420px" }, 0)
         }
 
         // $("#box1").animate({ "bottom": "+=420px" }, computerSpeed-100)
-        $("#box2").animate({ "bottom": "+=420px" }, mySpeed-100)
+        $("#box2").animate({ "bottom": "+=420px" }, mySpeed - 100)
 
         // if (mySpeed > computerSpeed) {
         //     mC++    
         //    document.getElementById("myCounter").innerHTML = mC;
- 
+
         // }
 
         // else if (computerSpeed > mySpeed) {
@@ -34,117 +37,121 @@ $(document).ready(function () {
         //     cC++
         //    document.getElementById("myCounter").innerHTML = mC;
         //     document.getElementById("computerCounter").innerHTML = cC;
-    
+
         // }
 
     })
-})
-
-
-
-$("#play").click(function () {
-
-
-playFunction();  
-startRobot();
-
-})
 
 
 
 
+    $("#play").click(function () {
 
 
-function playFunction(){
+        playFunction();
+        
+
+    })
+
+
+
+
+
+
+    function playFunction() {
         var x = setInterval(countDown, 1000)
+        var y = setInterval(startRobot,1000)
 
-    var seconds = 3;
-    document.getElementById("countdown").innerHTML = seconds
+        var seconds = 10;
+        document.getElementById("countdown").innerHTML = seconds
 
-    function countDown() {
-        seconds--;
-        document.getElementById("countdown").innerHTML = seconds;
+        function countDown() {
+            console.log('coundown')
+            seconds--;
+            document.getElementById("countdown").innerHTML = seconds;
 
             if (seconds < 0) {
+                console.log('countdown stopped')
                 clearInterval(x);
+                clearInterval(y);
                 $("#start").attr("disabled", "true")
                 announceWinner();
             }
-    } 
+        }
 
-}
+    }
 
-function startRobot(){
-var computerSpeed = Math.random() * 1000;
-        // var mySpeed = Math.random() * 1000;
 
-        if (mC != 0  || cC != 0)
-        {
+    function startRobot() {
+        
+        var computerSpeed = Math.random() * 1000;
+       
+
+        let cC = document.getElementById("computerCounter").innerHTML;
+        
+
+        if (mC != 0 || cC != 0) {
             $("#box1").animate({ "bottom": "-=420px" }, 0)
             // $("#box2").animate({ "bottom": "-=420px" }, 0)
         }
 
-        $("#box1").animate({ "bottom": "+=420px" }, computerSpeed-100)
+        $("#box1").animate({ "bottom": "+=420px" }, computerSpeed - 100)
         // $("#box2").animate({ "bottom": "+=420px" }, mySpeed-100)
 
-    
-compareSpeed();
-    
+
+       
 
 
-}
 
-function compareSpeed(){
+    }
+
+    function compareSpeed() {
 
         if (mySpeed > computerSpeed) {
-            mC++    
-           document.getElementById("myCounter").innerHTML = mC;
- 
+            mC++
+            document.getElementById("myCounter").innerHTML = mC;
+
         }
 
         else if (computerSpeed > mySpeed) {
             cC++
-          document.getElementById("computerCounter").innerHTML = cC;
-         }
+            document.getElementById("computerCounter").innerHTML = cC;
+        }
         else {
             mC++
             cC++
-           document.getElementById("myCounter").innerHTML = mC;
+            document.getElementById("myCounter").innerHTML = mC;
             document.getElementById("computerCounter").innerHTML = cC;
-    
+
         }
 
-}
-
-function announceWinner() {
-
-    let mC = document.getElementById("myCounter").innerHTML;
-    let cC = document.getElementById("computerCounter").innerHTML;
-
-    if (mC > cC) {
-        // alert("you won")
-        $('.boundries').append("<div class='messageWon'> <h1>You Won</h1> </div>");
-
     }
 
-    else if (cC > mC) {
-        // alert("you lost")
-        $('.boundries').append("<div class='messageLost'> <h1>You lost to a robot</h1> </div>");
+    function announceWinner() {
 
+        let mC = document.getElementById("myCounter").innerHTML;
+        let cC = document.getElementById("computerCounter").innerHTML;
+
+        if (mC > cC) {
+            
+            $('.boundries').append("<div class='messageWon'> <h1>You Won</h1> </div>");
+
+        }
+
+        else if (cC > mC) {
+            
+            $('.boundries').append("<div class='messageLost'> <h1>You lost to a robot</h1> </div>");
+
+        }
+
+        else {
+            
+            $('.boundries').append("<div class='messageTry'> <h1>You didn't even try</h1> </div>");
+        }
     }
 
-    else {
-        // alert("try again")
-        $('.boundries').append("<div class='messageTry'> <h1>You didn't even try</h1> </div>");
-    }
-}
 
 
 
-
-
-
-
-
-
+})
 
